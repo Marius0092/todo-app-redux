b3d3f1704890ff36e55a44cd4249f32f65bd38d6
Mario Montante <mariom0092@gmail.com>
2023-10-18 01:52:28 +0200

    added style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import { useState } from 'react';
 import { deleteTodo, toggleTodo, addTodo } from './actions';
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const todos = useSelector(state => state.todos);
@@ -24,29 +26,49 @@ function App() {
   };
 
   return (
-    <div className='App'>
+    <div className='App d-flex flex-column justify-content-center align-items-center'>
       <h1>To-Do List</h1>
-      <ul>
+      <ul
+        className='d-flex flex-column align-items-end'
+        style={{
+          width: '230px',
+          margin: '0',
+          marginBottom: '10px',
+          padding: '0',
+          gap: '5px',
+        }}
+      >
         {todos.map(todo => (
-          <li key={todo.id}>
+          <li
+            className='d-flex flex-row justify-content-center align-items-center'
+            style={{ gap: '5px' }}
+            key={todo.id}
+          >
             <input
               type='checkbox'
               checked={todo.completed}
               onChange={() => handleToggleTodo(todo.id)}
             />
-            {todo.text}
-            {''}
-            <button onClick={() => handleDeleteTodo(todo.id)}>Delete</button>
+            {todo.text}{' '}
+            <Button
+              variant='danger'
+              size='sm'
+              onClick={() => handleDeleteTodo(todo.id)}
+            >
+              Delete
+            </Button>
           </li>
         ))}
       </ul>
-      <div>
+      <div className='d-flex flex-row justify-content-center align-items-center'>
         <input
           type='text'
           value={text}
           onChange={e => setText(e.target.value)}
         />
-        <button onClick={handleAddTodo}>Add</button>
+        <Button variant='primary' size='sm' onClick={handleAddTodo}>
+          Add
+        </Button>
       </div>
     </div>
   );
